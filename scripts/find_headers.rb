75ece11fc1a436e9026a83b5f7a06c130218ed31
Dasmer Singh <dasmer.singh@venmo.com>
2015-04-16 13:15:46 -0400

    Update syntax and add argument exception

--- scripts/find_headers.rb
+++ scripts/find_headers.rb
@@ -6,21 +6,23 @@ require 'optparse'
 
 ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
-
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: find_headers.rb [options]"
+  opts.banner = 'Usage: find_headers.rb [options]'
 
-  opts.on("--public", "Find public headers") do |v|
+  opts.on('--public', 'Find public headers') do |v|
     options[:public] = v
   end
 
-  opts.on("--private", "Find private headers") do |v|
+  opts.on('--private', 'Find private headers') do |v|
     options[:private] = v
   end
 end.parse!
 
-separator = ARGV.shift || "\n"
+required_opts = options[:public] || options[:private]
+fail ArgumentError, 'Must provide --public or --private' unless required_opts
+
+separator = "\n"
 
 project = Xcodeproj::Project.open(ROOT + 'ResearchKit.xcodeproj')
 target = project.targets.find { |t| t.name == 'ResearchKit' }
@@ -37,12 +39,12 @@ end
 
 if options[:public]
   puts public_headers.map { |build_file|
-    build_file.file_ref.real_path.relative_path_from ROOT
-  }.to_a.join(separator)
+    build_file.file_ref.real_path.relative_path_from(ROOT)
+  }.join(separator)
 end
 
 if options[:private]
   puts private_headers.map { |build_file|
-    build_file.file_ref.real_path.relative_path_from ROOT
-  }.to_a.join(separator)
+    build_file.file_ref.real_path.relative_path_from(ROOT)
+  }.join(separator)
 end
