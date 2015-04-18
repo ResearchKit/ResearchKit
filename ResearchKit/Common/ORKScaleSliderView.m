62cff57325e60b136386e2f269e5f14c2d19c66e
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2015-04-19 00:06:15 +0100

    Add copyright line to ORKScaleSlider & ORKScaleSliderView

--- ResearchKit/Common/ORKScaleSliderView.m
+++ ResearchKit/Common/ORKScaleSliderView.m
@@ -1,6 +1,7 @@
 /*
  Copyright (c) 2015, Apple Inc. All rights reserved.
- 
+ Copyright (c) 2015, Ricardo Sánchez-Sáez.
+
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
  
