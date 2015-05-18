1958f073db24e59e05fadae4f2a59e95c321f83d
Alex Basson <alexbasson@gmail.com>
2015-05-17 20:26:57 -0400

    Add tests for ORKConsentSectionFormatter
    * reorganize consent-related tests into Consent group

--- ResearchKitTests/ORKConsentSectionFormatterTests.m
+++ ResearchKitTests/ORKConsentSectionFormatterTests.m
@@ -1,40 +1,46 @@
-//
-//  ORKConsentSectionFormatterTests.m
-//  ResearchKit
-//
-//  Created by Alex on 5/17/15.
-//  Copyright (c) 2015 researchkit.org. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
 #import <XCTest/XCTest.h>
+#import "ORKConsentSectionFormatter.h"
+#import "ORKConsentSection_Internal.h"
 
 @interface ORKConsentSectionFormatterTests : XCTestCase
-
+@property (nonatomic, strong) ORKConsentSectionFormatter *formatter;
+@property (nonatomic, strong) ORKConsentSection *section;
 @end
 
 @implementation ORKConsentSectionFormatterTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.formatter = [[ORKConsentSectionFormatter alloc] init];
+    self.section = [[ORKConsentSection alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.formatter = nil;
+    self.section = nil;
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (void)testHTMLForSection_whenSectionHasFormalTitle_formatsFormalTitle {
+    self.section.formalTitle = @"Formal Title";
+    self.section.title = @"Informal Title";
+    XCTAssertEqualObjects([self.formatter HTMLForSection:self.section], @"<h4>Formal Title</h4><p></p>");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testHTMLForSection_whenSectionHasNoFormalTitle_formatsFormalTitle {
+    self.section.title = @"Informal Title";
+    XCTAssertEqualObjects([self.formatter HTMLForSection:self.section], @"<h4>Informal Title</h4><p></p>");
+}
+
+- (void)testHTMLForSection_whenSectionHasHTMLContent_formatsHTMLContent {
+    self.section.htmlContent = @"html content";
+    self.section.content = @"other content";
+    XCTAssertEqualObjects([self.formatter HTMLForSection:self.section], @"<h4></h4><p>html content</p>");
+}
+
+- (void)testHTMLForSection_whenSectionHasNoHTMLContent_formatsEscapedContent {
+    self.section.content = @"unescaped content\nwith special characters such as < >";
+    XCTAssertEqualObjects([self.formatter HTMLForSection:self.section], @"<h4></h4><p>unescaped content<br/>with special characters such as &lt; &gt;</p>");
 }
 
 @end
