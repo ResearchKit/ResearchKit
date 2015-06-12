4b3b2a64e1532683288511bdd52b845262314cca
Julien Thérier <julien.therier@shazino.com>
2015-06-12 09:47:13 +0200

    Remove answer 2 and 18 (no longer relevant)

--- ResearchKit/ActiveTasks/ORKPVSATKeyboardView.m
+++ ResearchKit/ActiveTasks/ORKPVSATKeyboardView.m
@@ -48,7 +48,7 @@
     if (self) {
         NSMutableArray *buttonsArray = [[NSMutableArray alloc] initWithCapacity:17];
         ORKBorderedButton *answerButton = nil;
-        for (NSUInteger i = 2; i <= 18; i++) {
+        for (NSUInteger i = 3; i <= 17; i++) {
             answerButton = [self answerButtonWithTitle:@(i).stringValue];
             [buttonsArray addObject:answerButton];
             [self addSubview:answerButton];
@@ -85,61 +85,46 @@
     
     NSMutableArray *constraintsArray = [NSMutableArray array];
     
-    ORKBorderedButton *answer2Button = self.answerButtons[0];
-    ORKBorderedButton *answer3Button = self.answerButtons[1];
-    ORKBorderedButton *answer4Button = self.answerButtons[2];
-    ORKBorderedButton *answer5Button = self.answerButtons[3];
-    ORKBorderedButton *answer6Button = self.answerButtons[4];
-    ORKBorderedButton *answer7Button = self.answerButtons[5];
-    ORKBorderedButton *answer8Button = self.answerButtons[6];
-    ORKBorderedButton *answer9Button = self.answerButtons[7];
-    ORKBorderedButton *answer10Button = self.answerButtons[8];
-    ORKBorderedButton *answer11Button = self.answerButtons[9];
-    ORKBorderedButton *answer12Button = self.answerButtons[10];
-    ORKBorderedButton *answer13Button = self.answerButtons[11];
-    ORKBorderedButton *answer14Button = self.answerButtons[12];
-    ORKBorderedButton *answer15Button = self.answerButtons[13];
-    ORKBorderedButton *answer16Button = self.answerButtons[14];
-    ORKBorderedButton *answer17Button = self.answerButtons[15];
-    ORKBorderedButton *answer18Button = self.answerButtons[16];
+    ORKBorderedButton *answer3Button = self.answerButtons[0];
+    ORKBorderedButton *answer4Button = self.answerButtons[1];
+    ORKBorderedButton *answer5Button = self.answerButtons[2];
+    ORKBorderedButton *answer6Button = self.answerButtons[3];
+    ORKBorderedButton *answer7Button = self.answerButtons[4];
+    ORKBorderedButton *answer8Button = self.answerButtons[5];
+    ORKBorderedButton *answer9Button = self.answerButtons[6];
+    ORKBorderedButton *answer10Button = self.answerButtons[7];
+    ORKBorderedButton *answer11Button = self.answerButtons[8];
+    ORKBorderedButton *answer12Button = self.answerButtons[9];
+    ORKBorderedButton *answer13Button = self.answerButtons[10];
+    ORKBorderedButton *answer14Button = self.answerButtons[11];
+    ORKBorderedButton *answer15Button = self.answerButtons[12];
+    ORKBorderedButton *answer16Button = self.answerButtons[13];
+    ORKBorderedButton *answer17Button = self.answerButtons[14];
+
     
-    NSDictionary *views = NSDictionaryOfVariableBindings(answer2Button, answer3Button, answer4Button, answer5Button, answer6Button, answer7Button, answer8Button, answer9Button, answer10Button, answer11Button, answer12Button, answer13Button, answer14Button, answer15Button, answer16Button, answer17Button, answer18Button);
+    NSDictionary *views = NSDictionaryOfVariableBindings(answer3Button, answer4Button, answer5Button, answer6Button, answer7Button, answer8Button, answer9Button, answer10Button, answer11Button, answer12Button, answer13Button, answer14Button, answer15Button, answer16Button, answer17Button);
     
     // First line of answer buttons
     [constraintsArray addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[answer2Button]-[answer3Button(==answer2Button)]-[answer4Button(==answer2Button)]-[answer5Button(==answer2Button)]-[answer6Button(==answer2Button)]-[answer7Button(==answer2Button)]-|"
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[answer3Button]-[answer4Button(==answer3Button)]-[answer5Button(==answer3Button)]-[answer6Button(==answer3Button)]-[answer7Button(==answer3Button)]-|"
                                              options:NSLayoutFormatAlignAllCenterY|NSLayoutFormatAlignAllTop|NSLayoutFormatAlignAllBottom
                                              metrics:nil views:views]];
     
     // Second line of answer buttons
     [constraintsArray addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:[answer8Button]-[answer9Button(==answer8Button)]-[answer10Button(==answer8Button)]-[answer11Button(==answer8Button)]-[answer12Button(==answer8Button)]"
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[answer8Button]-[answer9Button(==answer8Button)]-[answer10Button(==answer8Button)]-[answer11Button(==answer8Button)]-[answer12Button(==answer8Button)]-|"
                                              options:NSLayoutFormatAlignAllCenterY|NSLayoutFormatAlignAllTop|NSLayoutFormatAlignAllBottom
                                              metrics:nil views:views]];
     
-    [constraintsArray addObject:[NSLayoutConstraint constraintWithItem:answer8Button
-                                                        attribute:NSLayoutAttributeWidth
-                                                        relatedBy:NSLayoutRelationEqual
-                                                           toItem:answer2Button
-                                                        attribute:NSLayoutAttributeWidth
-                                                       multiplier:1.f constant:0.f]];
-    
-    [constraintsArray addObject:[NSLayoutConstraint constraintWithItem:answer10Button
-                                                        attribute:NSLayoutAttributeCenterX
-                                                        relatedBy:NSLayoutRelationEqual
-                                                           toItem:self
-                                                        attribute:NSLayoutAttributeCenterX
-                                                       multiplier:1.f constant:0.f]];
-    
     // Third line of answer buttons
     [constraintsArray addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[answer13Button]-[answer14Button(==answer13Button)]-[answer15Button(==answer13Button)]-[answer16Button(==answer13Button)]-[answer17Button(==answer13Button)]-[answer18Button(==answer13Button)]-|"
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[answer13Button]-[answer14Button(==answer13Button)]-[answer15Button(==answer13Button)]-[answer16Button(==answer13Button)]-[answer17Button(==answer13Button)]-|"
                                              options:NSLayoutFormatAlignAllCenterY|NSLayoutFormatAlignAllTop|NSLayoutFormatAlignAllBottom
                                              metrics:nil views:views]];
     
     // Align vertically
     [constraintsArray addObjectsFromArray:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[answer2Button]-[answer8Button(==answer2Button)]-[answer13Button(==answer2Button)]-|"
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[answer3Button]-[answer8Button(==answer3Button)]-[answer13Button(==answer3Button)]-|"
                                              options:(NSLayoutFormatOptions)0
                                              metrics:nil views:views]];
     
