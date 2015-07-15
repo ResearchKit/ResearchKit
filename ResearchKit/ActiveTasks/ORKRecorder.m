0f1a234076674548c1aab6e29d12f160ef125e9d
Umer Khan <umer_khan@Umers-MacBook-Pro.local>
2015-07-15 15:50:29 -0700

    Removed identifiers from logName and added recorderUUID.

--- ResearchKit/ActiveTasks/ORKRecorder.m
+++ ResearchKit/ActiveTasks/ORKRecorder.m
@@ -178,7 +178,7 @@
 }
 
 - (NSString *)logName {
-    return [NSString stringWithFormat:@"%@_%@_%@", [self recorderType], self.identifier, self.step.identifier];
+    return [NSString stringWithFormat:@"%@_%@", [self recorderType], _recorderUUID];
 }
 
 - (ORKDataLogger *)makeJSONDataLoggerWithError:(NSError * __autoreleasing *)error {
