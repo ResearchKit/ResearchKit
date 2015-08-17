52aed1a5b6056ab98866c19df05673d805c69bce
Pascal Pfiffner <phase.of.matter@gmail.com>
2015-08-17 16:58:33 +0200

    Use section title in “Learn More” screen

--- ResearchKit/Consent/ORKConsentSceneViewController.m
+++ ResearchKit/Consent/ORKConsentSceneViewController.m
@@ -198,7 +198,7 @@ static NSString *localizedLearnMoreForType(ORKConsentSectionType sectionType) {
     }else{
         viewController = [[ORKConsentLearnMoreViewController alloc] initWithHTMLContent:((_section.htmlContent.length > 0) ? _section.htmlContent : _section.escapedContent)];
     }
-    viewController.title = ORKLocalizedString(@"CONSENT_LEARN_MORE_TITLE", nil);
+	viewController.title = _section.title ?: ORKLocalizedString(@"CONSENT_LEARN_MORE_TITLE", nil);
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
     navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
     [self presentViewController:navigationController animated:YES completion:nil];
