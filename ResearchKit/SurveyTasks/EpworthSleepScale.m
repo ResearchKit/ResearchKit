ce490ef445ac2b696e5443789a2a13be21d705a5
Andrew Hill <andrew.hill@doctors.org.uk>
2015-09-13 15:26:56 +0100

    Epworth sleep scale added including feedback on results.

--- ResearchKit/SurveyTasks/EpworthSleepScale.m
+++ ResearchKit/SurveyTasks/EpworthSleepScale.m
@@ -19,6 +19,7 @@
 #import "EpworthSleepScale.h"
 #import "ORKDefines_Private.h"
 #import "ORKOrderedTask.h"
+#import "ORKCompletionStep.h"
 
 @implementation EpworthSleepScale
 
@@ -59,8 +60,42 @@ static void ORKStepArrayAddStep(NSMutableArray *array, ORKStep *step) {
         ORKStepArrayAddStep(steps,step);
     }
     
+    ORKCompletionStep *finalStep = [[ORKCompletionStep alloc] initWithIdentifier:@"FinalStep"];
+    ORKStepArrayAddStep(steps,finalStep);
+
+    
     self = [super initWithIdentifier:identifier steps:steps];
     return self;
 }
+
+- (ORKStep *)stepAfterStep:(ORKStep *)step withResult:(ORKTaskResult *)result {
+
+    ORKStep *nextStep = [super stepAfterStep:step withResult:result];
+    
+    if ([nextStep.identifier isEqualToString:@"FinalStep"]) {
+        int epworthScore = 0;
+        for (int i=0;i<8;i++) {
+            ORKStepResult *itemResult = (ORKStepResult *)result.results[i];
+            if (itemResult.results.count > 0) {
+                if ([itemResult.results[0] isKindOfClass:[ORKChoiceQuestionResult class]]) {
+                    ORKChoiceQuestionResult *choiceResultArray = (ORKChoiceQuestionResult *) itemResult.results[0];
+                     NSNumber *choiceValue = (NSNumber *) choiceResultArray.choiceAnswers[0];
+                    epworthScore += [choiceValue intValue];
+                }
+            }
+        }
+        nextStep.title = ORKLocalizedString(@"EPWORTH_FINAL_TITLE", nil);
+        if (epworthScore > 12) {
+            nextStep.text = ORKLocalizedString(@"EPWORTH_RESULT_2", nil);
+        } else if (epworthScore > 9) {
+            nextStep.text = ORKLocalizedString(@"EPWORTH_RESULT_1", nil);
+        } else {
+            nextStep.text = ORKLocalizedString(@"EPWORTH_RESULT_0", nil);
+        }
+    }
+
+    return nextStep;
+}
+
 
 @end
