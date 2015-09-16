9b1513915c0b149aa88ac0ad455d3c811c7a1caf
Andrew Hill <andrew.hill@doctors.org.uk>
2015-09-16 23:16:23 +0100

    Revert "Changed to ValidatedSurveys from PrebuiltSurveys"
    This reverts commit 6a8aa182b3fdf7cc7df029eb14a7be5b41688c57.

---
+++ ResearchKit/SurveyTasks/ORKRivermeadMobilityIndex.m
@@ -0,0 +1,79 @@
+/*
+ Copyright (c) 2015, Dr. Andrew Hill.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#import "ORKRivermeadMobilityIndex.h"
+#import "ORKDefines_Private.h"
+#import "ORKOrderedTask.h"
+
+
+@implementation ORKRivermeadMobilityIndex
+
+static void ORKStepArrayAddStep(NSMutableArray *array, ORKStep *step) {
+    [step validateParameters];
+    [array addObject:step];
+}
+
+- (instancetype)initWithIdentifier:(NSString *)identifier {
+    
+    NSMutableArray *steps = [NSMutableArray array];
+    
+    // Explain the purpose of the introduction survey
+    {
+        ORKInstructionStep *step = [[ORKInstructionStep alloc] initWithIdentifier:@"IntroStep"];
+        step.title = ORKLocalizedString(@"RIVERMEAD_INTRO_TITLE", nil);
+        step.text = ORKLocalizedString(@"RIVERMEAD_INTRO_TEXT", nil);
+        ORKStepArrayAddStep(steps, step);
+    }
+    
+    NSArray *answerChoices = [NSArray arrayWithObjects:
+                              [[ORKTextChoice alloc] initWithText:ORKLocalizedString(@"RIVERMEAD_ANSWER_YES", nil) detailText:nil value:[NSNumber numberWithInt:1] exclusive:true],
+                              [[ORKTextChoice alloc] initWithText:ORKLocalizedString(@"RIVERMEAD_ANSWER_NO", nil) detailText:nil value:[NSNumber numberWithInt:0] exclusive:true],
+                              nil];
+    
+    ORKTextChoiceAnswerFormat *answerFormat = [[ORKTextChoiceAnswerFormat alloc] initWithStyle:ORKChoiceAnswerStyleSingleChoice textChoices:answerChoices];
+    
+    for (int questionID=0; questionID<15; questionID++) {
+        ORKQuestionStep *step = [[ORKQuestionStep alloc] initWithIdentifier:[NSString stringWithFormat:@"%d",questionID]];
+        step.title = ORKLocalizedString(@"RIVERMEAD_TITLE", nil);
+        {
+            NSString *questionStem = [NSString stringWithFormat:@"RIVERMEAD_Q%d",questionID];
+            step.text = ORKLocalizedString(questionStem, nil);
+            step.optional = NO;
+        }
+        step.answerFormat = answerFormat;
+        ORKStepArrayAddStep(steps,step);
+    }
+    
+    self = [super initWithIdentifier:identifier steps:steps];
+    return self;
+}
+
+@end
