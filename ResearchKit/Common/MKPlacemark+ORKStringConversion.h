a8f6be61f3b28f4b7bceba5d689ba9b7fa4cd153
Steve Cadwallader <steve.cadwallader@gmail.com>
2015-10-09 08:38:43 -0400

    Copyright updates

--- ResearchKit/Common/MKPlacemark+ORKStringConversion.h
+++ ResearchKit/Common/MKPlacemark+ORKStringConversion.h
@@ -4,10 +4,11 @@
  Copyright (c) 2015, Bryan Strothmann, Quintiles Inc.
  Copyright (c) 2015, Greg Yip, Quintiles Inc.
  Copyright (c) 2015, John Reites, Quintiles Inc.
+ Copyright (c) 2015, Pavel Kanzelsberger, Quintiles Inc.
  Copyright (c) 2015, Richard Thomas, Quintiles Inc.
  Copyright (c) 2015, Shelby Brooks, Quintiles Inc.
- Copyright (c) 2015, Pavel Kanzelsberger, Quintiles Inc.
- 
+ Copyright (c) 2015, Steve Cadwallader, Quintiles Inc.
+
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
  
