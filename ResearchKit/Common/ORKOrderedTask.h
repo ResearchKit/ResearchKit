8083ccd5c637c23cd1ca0479aaa53158d3b07343
Shannon Young <shannon.young@sagebase.org>
2016-03-11 13:41:26 -0800

    Move the two-handed tapping test into ResearchKit

--- ResearchKit/Common/ORKOrderedTask.h
+++ ResearchKit/Common/ORKOrderedTask.h
@@ -307,6 +307,33 @@ typedef NS_OPTIONS(NSUInteger, ORKTremorActiveTaskOption) {
                                         intendedUseDescription:(nullable NSString *)intendedUseDescription
                                                       duration:(NSTimeInterval)duration
                                                        options:(ORKPredefinedTaskOption)options;
+/**
+ Returns a predefined task that consists of two finger tapping (Optionally with a hand specified)
+ 
+ In a two finger tapping task, the participant is asked to rhythmically and alternately tap two
+ targets on the device screen.
+ 
+ A two finger tapping task can be used to assess basic motor capabilities including speed, accuracy,
+ and rhythm.
+ 
+ Data collected in this task includes touch activity and accelerometer information.
+ 
+ @param identifier              The task identifier to use for this task, appropriate to the study.
+ @param intendedUseDescription  A localized string describing the intended use of the data
+ collected. If the value of this parameter is `nil`, the default
+ localized text will be displayed.
+ @param duration                The length of the count down timer that runs while touch data is
+ collected.
+ @param options                 Options that affect the features of the predefined task.
+ @param handOptions             Options for determining which hand(s) to test.
+ 
+ @return An active two finger tapping task that can be presented with an `ORKTaskViewController` object.
+ */
++ (ORKOrderedTask *)twoFingerTappingIntervalTaskWithIdentifier:(NSString *)identifier
+                                        intendedUseDescription:(nullable NSString *)intendedUseDescription
+                                                      duration:(NSTimeInterval)duration
+                                                       options:(ORKPredefinedTaskOption)options
+                                                   handOptions:(ORKPredefinedTaskHandOption)handOptions;
 
 /**
  Returns a predefined task that tests spatial span memory.
