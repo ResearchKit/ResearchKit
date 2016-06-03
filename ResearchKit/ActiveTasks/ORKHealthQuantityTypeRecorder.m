e8679a076fdb3d43d55a823d2e3cb08f01c6978b
Yuan Zhu <y_zhu@apple.com>
2016-06-03 14:57:26 -0700

    Remove commented code

--- ResearchKit/ActiveTasks/ORKHealthQuantityTypeRecorder.m
+++ ResearchKit/ActiveTasks/ORKHealthQuantityTypeRecorder.m
@@ -128,25 +128,6 @@ static const NSInteger _HealthAnchoredQueryLimit = 100;
     NSAssert(_samplePredicate != nil, @"Sample predicate should be non-nil if recording");
     
     __weak typeof(self) weakSelf = self;
-    /*
-    HKAnchoredObjectQuery *anchoredQuery = [[HKAnchoredObjectQuery alloc]
-                                            initWithType:_quantityType
-                                            predicate:_samplePredicate
-                                            anchor:_anchor
-                                            limit:_HealthAnchoredQueryLimit
-                                            completionHandler:^(HKAnchoredObjectQuery *query, NSArray *results, NSUInteger newAnchor, NSError *error)
-                                            {
-                                                if (error) {
-                                                    // An error in the query's not the end of the world: we'll probably get another chance. Just log it.
-                                                    ORK_Log_Warning(@"Anchored query error: %@", error);
-                                                    return;
-                                                }
-                                                
-                                                __typeof(self) strongSelf = weakSelf;
-                                                [strongSelf query_logResults:results withAnchor:newAnchor];
-                                                
-                                            }];
-    */
     
     HKAnchoredObjectQuery *anchoredQuery = [[HKAnchoredObjectQuery alloc]
                                             initWithType:_quantityType
