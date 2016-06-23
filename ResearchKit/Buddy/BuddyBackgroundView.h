e567d1a9e8e8133936283f35756d65dc5ece2948
Atte Keltanen <atte.keltanen@digital-identity.com>
2016-06-23 11:40:25 +0300

    add background gradient view

---
+++ ResearchKit/Buddy/BuddyBackgroundView.h
@@ -0,0 +1,19 @@
+//
+//  BuddyBackgroundView.h
+//  ResearchKit
+//
+//  Created by Atte Keltanen on 23/06/16.
+//  Copyright © 2016 researchkit.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ResearchKit/ResearchKit.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BuddyBackgroundView: UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
