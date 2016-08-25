2c5aefae182d0e9d6cca14489eac3da02d2bd776
Sam Lichlyter <slichlyter12@me.com>
2016-08-25 08:44:37 -0700

    Added Gesture Recognizer to MapView for ORKLocationAnswerFormat View.
    Press and hold on map for one second to place marker and input location into text field.

--- ResearchKit/Common/ORKLocationSelectionView.m
+++ ResearchKit/Common/ORKLocationSelectionView.m
@@ -141,6 +141,7 @@ static const NSString *FormattedAddressLines = @"FormattedAddressLines";
             [self addSubview:_seperator3];
         }
         
+        [self setUpGestureRecognizer];
         [self setUpConstraints];
 
         if (NO == formMode) {
@@ -151,6 +152,27 @@ static const NSString *FormattedAddressLines = @"FormattedAddressLines";
     }
     
     return self;
+}
+
+- (void)setUpGestureRecognizer {
+    UILongPressGestureRecognizer *lpgr = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(addPlacemarkToMap:)];
+    lpgr.minimumPressDuration = 1.0; // press for 1 second
+    [_mapView addGestureRecognizer:lpgr];
+}
+
+- (void)addPlacemarkToMap:(UIGestureRecognizer *)gestureRecognizer {
+    if (gestureRecognizer.state != UIGestureRecognizerStateBegan)
+        return;
+    
+    CGPoint touchPoint = [gestureRecognizer locationInView:_mapView];
+    CLLocationCoordinate2D touchMapCoordinate = [_mapView convertPoint:touchPoint toCoordinateFromView:_mapView];
+    
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    annotation.coordinate = touchMapCoordinate;
+    [_mapView addAnnotation:annotation];
+    
+    ORKLocation *pinLocation = [[ORKLocation alloc] initWithCoordinate:touchMapCoordinate region:nil userInput:nil addressDictionary:nil];
+    [self setAnswer:pinLocation];
 }
 
 - (void)setUpConstraints {
