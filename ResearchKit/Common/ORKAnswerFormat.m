d0c261b36b2295aecd2fd87bd2a24025ded81e38
Fernando Waigandt <mirlhaq@gmail.com>
2016-11-02 13:36:33 -0300

    Medable - Fixes #834. "Continuous Scale Step - UI value and result value mismatch when using fraction digits."

--- ResearchKit/Common/ORKAnswerFormat.m
+++ ResearchKit/Common/ORKAnswerFormat.m
@@ -453,6 +453,19 @@ NSNumberFormatterStyle ORKNumberFormattingStyleConvert(ORKNumberFormattingStyle 
 
 - (ORKQuestionResult *)resultWithIdentifier:(NSString *)identifier answer:(id)answer {
     ORKQuestionResult *questionResult = [[[self questionResultClass] alloc] initWithIdentifier:identifier];
+    
+// Medable ---
+/*
+    ContinuousScale navigation rules always evaluate to false because the result is different from what is displayed in the UI.
+    The fraction digits have to be taken into account in self.answer as well.
+*/
+    if ([self isKindOfClass:[ORKContinuousScaleAnswerFormat class]])
+    {
+        NSNumberFormatter* formatter = [(ORKContinuousScaleAnswerFormat*)self numberFormatter];
+        answer = [formatter numberFromString:[formatter stringFromNumber:answer]];
+    }
+// Medable ---
+    
     questionResult.answer = answer;
     questionResult.questionType = self.questionType;
     return questionResult;
