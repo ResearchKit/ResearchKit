e9569123fd73220ed86ad414b4a6ae296ab00829
Fernando Waigandt <mirlhaq@gmail.com>
2017-01-25 21:35:38 -0300

    Pull request #899 - Requested changes - Added Medable Copyright.

--- ResearchKit/Common/ORKAnswerFormat.m
+++ ResearchKit/Common/ORKAnswerFormat.m
@@ -2,6 +2,7 @@
  Copyright (c) 2015, Apple Inc. All rights reserved.
  Copyright (c) 2015, Scott Guelich.
  Copyright (c) 2016, Ricardo Sánchez-Sáez.
+ Copyright (c) 2017, Medable Inc. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
