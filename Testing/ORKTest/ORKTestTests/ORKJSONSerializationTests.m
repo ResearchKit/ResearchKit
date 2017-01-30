2986d85f58f1f9a31ca9918dd951b114b6e7fdb8
Shannon Young <shannon.young@sagebase.org>
2017-01-30 13:10:16 -0800

    Added equality testing for primitive double, int and bool properties
    This adds equality checking for primitive types for the double, int and bool properties.
    
    Note: this *only* checks equality and does not test these properties for encoding, though I have also fixed the encoding for those classes that failed the equality tests.

--- Testing/ORKTest/ORKTestTests/ORKJSONSerializationTests.m
+++ Testing/ORKTest/ORKTestTests/ORKJSONSerializationTests.m
@@ -42,6 +42,7 @@
 @property (nonatomic, copy) NSString *propertyName;
 @property (nonatomic, strong) Class propertyClass;
 @property (nonatomic) BOOL isPrimitiveType;
+@property (nonatomic) BOOL isBoolType;
 
 - (instancetype)initWithObjcProperty:(objc_property_t)property;
 
@@ -76,6 +77,10 @@
            
         } else if ([@[@"Ti", @"Tq", @"TI", @"TQ"] containsObject:typeAttribute]) {
             self.propertyClass = [NSNumber class];
+        }
+        else if ([typeAttribute isEqualToString:@"TB"]) {
+            self.propertyClass = [NSNumber class];
+            _isBoolType = YES;
         }
     }
     return self;
@@ -540,6 +545,19 @@ ORK_MAKE_TEST_INIT(CLCircularRegion, (^{
 - (BOOL)applySomeValueToClassProperty:(ClassProperty *)p forObject:(id)instance index:(NSInteger)index forEqualityCheck:(BOOL)equality {
     // return YES if the index makes it distinct
     
+    if (p.isPrimitiveType) {
+        if (p.propertyClass == [NSNumber class]) {
+            if (p.isBoolType) {
+                XCTAssertNoThrow([instance setValue:index?@YES:@NO forKey:p.propertyName]);
+            } else {
+                XCTAssertNoThrow([instance setValue:index?@(12):@(123) forKey:p.propertyName]);
+            }
+            return YES;
+        } else {
+            return NO;
+        }
+    }
+    
     Class aClass = [instance class];
     // Assign value to object type property
     if (p.propertyClass == [NSObject class] && (aClass == [ORKTextChoice class]|| aClass == [ORKImageChoice class] || (aClass == [ORKQuestionResult class])))
@@ -805,13 +823,28 @@ ORK_MAKE_TEST_INIT(CLCircularRegion, (^{
                                        @"descriptionSuffix",
                                        @"debugDescription",
                                        @"hash",
-                                       @"requestedHealthKitTypesForReading",
-                                       @"healthKitUnit",
-                                       @"requestedHealthKitTypesForWriting",
+                                       
+                                       // ResearchKit specific
                                        @"answer",
                                        @"firstResult",
-                                       @"ORKPageStep.steps",
+                                       @"healthKitUnit",
+                                       @"providesBackgroundAudioPrompts",
+                                       @"questionType",
+                                       @"requestedHealthKitTypesForReading",
+                                       @"requestedHealthKitTypesForWriting",
+                                       @"requestedPermissions",
+                                       @"shouldReportProgress",
+                                       
+                                       // For a specific class
+                                       @"ORKHeightAnswerFormat.useMetricSystem",
                                        @"ORKNavigablePageStep.steps",
+                                       @"ORKPageStep.steps",
+                                       @"ORKResult.saveable",
+                                       @"ORKReviewStep.isStandalone",
+                                       @"ORKStep.allowsBackNavigation",
+                                       @"ORKStep.restorable",
+                                       @"ORKStep.showsProgress",
+                                       @"ORKStepResult.isPreviousResult",
                                        ];
     
     // Test Each class
@@ -829,10 +862,8 @@ ORK_MAKE_TEST_INIT(CLCircularRegion, (^{
             NSString *dottedPropertyName = [NSString stringWithFormat:@"%@.%@",NSStringFromClass(aClass),p.propertyName];
             if ([propertyExclusionList containsObject: p.propertyName] == NO &&
                 [propertyExclusionList containsObject: dottedPropertyName] == NO) {
-                if (p.isPrimitiveType == NO) {
-                    if ([instance valueForKey:p.propertyName] == nil) {
-                        [self applySomeValueToClassProperty:p forObject:instance index:0 forEqualityCheck:YES];
-                    }
+                if (p.isPrimitiveType || [instance valueForKey:p.propertyName] == nil) {
+                    [self applySomeValueToClassProperty:p forObject:instance index:0 forEqualityCheck:YES];
                 }
                 [propertyNames addObject:p.propertyName];
             }
@@ -850,7 +881,6 @@ ORK_MAKE_TEST_INIT(CLCircularRegion, (^{
             NSString *dottedPropertyName = [NSString stringWithFormat:@"%@.%@",NSStringFromClass(aClass),p.propertyName];
             if ([propertyExclusionList containsObject: p.propertyName] == NO &&
                 [propertyExclusionList containsObject: dottedPropertyName] == NO) {
-                if (p.isPrimitiveType == NO) {
                     copiedInstance = [instance copy];
                     if (instance == copiedInstance) {
                         // Totally immutable object.
@@ -859,15 +889,16 @@ ORK_MAKE_TEST_INIT(CLCircularRegion, (^{
                     if ([self applySomeValueToClassProperty:p forObject:copiedInstance index:1 forEqualityCheck:YES])
                     {
                         if ([copiedInstance isEqual:instance]) {
-                            XCTAssertNotEqualObjects(copiedInstance, instance);
+                            XCTAssertNotEqualObjects(copiedInstance, instance, @"%@", p.propertyName);
                         }
                         [self applySomeValueToClassProperty:p forObject:copiedInstance index:0 forEqualityCheck:YES];
-                        XCTAssertEqualObjects(copiedInstance, instance);
+                        XCTAssertEqualObjects(copiedInstance, instance, @"%@", p.propertyName);
                         
-                        [copiedInstance setValue:nil forKey:p.propertyName];
-                        XCTAssertNotEqualObjects(copiedInstance, instance);
+                        if (p.isPrimitiveType == NO) {
+                            [copiedInstance setValue:nil forKey:p.propertyName];
+                            XCTAssertNotEqualObjects(copiedInstance, instance);
+                        }
                     }
-                }
             }
         }
     }
