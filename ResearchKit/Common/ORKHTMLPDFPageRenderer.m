c309d693492cbed94e00b6f84de5fa9ffc75454b
Chris Ortman <chris-ortman@uiowa.edu>
2017-04-26 22:13:47 -0500

    Update copyright

--- ResearchKit/Common/ORKHTMLPDFPageRenderer.m
+++ ResearchKit/Common/ORKHTMLPDFPageRenderer.m
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2016, Chris Ortman, University of Iowa, All rights reserved.
+ Copyright (c) 2017, Chris Ortman, University of Iowa, All rights reserved.
 
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
