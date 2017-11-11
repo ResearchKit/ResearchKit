60ccc4bfadb44658f593764434f2f9de0ea4152b
chrisnowak <chris@careevolution.com>
2017-11-11 09:33:55 -0500

    Adding base web view step

---
+++ ResearchKit/Common/ORKWebViewStepViewController.m
@@ -0,0 +1,82 @@
+/*
+ Copyright (c) 2017, CareEvolution, Inc.
+ 
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+ 
+ 1.  Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ 2.  Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation and/or
+ other materials provided with the distribution.
+ 
+ 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission. No license is granted to the trademarks of
+ the copyright holders even if such marks are included in this software.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "ORKWebViewStepViewController.h"
+#import "ORKWebViewStep.h"
+
+@implementation ORKWebViewStepViewController {
+    WKWebView *_webView;
+    NSString *_answer;
+}
+
+- (ORKWebViewStep *)webViewStep {
+    return (ORKWebViewStep *)self.step;
+}
+
+- (void)stepDidChange {
+    _answer = nil;
+    [_webView removeFromSuperview];
+    _webView = nil;
+    
+    if (self.step && [self isViewLoaded]) {
+        WKWebViewConfiguration* config = [[WKWebViewConfiguration alloc] init];
+        WKUserContentController *controller = [[WKUserContentController alloc] init];
+        [controller addScriptMessageHandler:self name:@"webViewStep"];
+        config.userContentController = controller;
+        _webView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+        [self.view addSubview:_webView];
+        NSURLRequest* request = [NSURLRequest requestWithURL:[self webViewStep].url];
+        [_webView loadRequest:request];
+        _webView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self stepDidChange];
+}
+
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message
+{
+    _answer = (NSString *)message.body;
+    [self goForward];
+}
+
+- (ORKStepResult *)result {
+    ORKStepResult *parentResult = [super result];
+    if (parentResult) {
+//        WebViewStepResult *childResult = [[WebViewStepResult alloc] initWithIdentifier:self.step.identifier];
+//        childResult.answer = _answer;
+//        parentResult.results = @[childResult];
+    }
+    return parentResult;
+}
+
+@end
