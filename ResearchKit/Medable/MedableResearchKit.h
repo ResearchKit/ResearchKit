7e8e6c415feeef35fdd2aa5af52400edba464cfa
Martin Adoue <martin@blackpixel.com>
2018-09-24 14:30:04 -0300

    Remove support for password strenght

--- ResearchKit/Medable/MedableResearchKit.h
+++ ResearchKit/Medable/MedableResearchKit.h
@@ -1,6 +1,5 @@
 // Medable Extra headers
 
-#import <ResearchKit/MDRTextInputFeedback.h>
 #import <ResearchKit/ORKQuestionStep.h>
 #import <ResearchKit/ORKQuestionStepView.h>
 #import <ResearchKit/ORKSurveyAnswerCell.h>
