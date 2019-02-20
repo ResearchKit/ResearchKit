6f883b4acb650eb79bed376873033eed4ba8f695
Ricardo Sanchez-Saez <rsanchezsaez@apple.com>
2019-02-19 19:25:36 -0800

    [ORKHealthAnswerFormat] Better static analyzer fixes

--- ResearchKit/Common/ORKHealthAnswerFormat.m
+++ ResearchKit/Common/ORKHealthAnswerFormat.m
@@ -46,7 +46,7 @@ ORKBiologicalSexIdentifier const ORKBiologicalSexIdentifierMale = @"HKBiological
 ORKBiologicalSexIdentifier const ORKBiologicalSexIdentifierOther = @"HKBiologicalSexOther";
 
 NSString *ORKHKBiologicalSexString(HKBiologicalSex biologicalSex) {
-    NSString *string = @"";
+    NSString *string = nil;
     switch (biologicalSex) {
         case HKBiologicalSexFemale: string = ORKBiologicalSexIdentifierFemale; break;
         case HKBiologicalSexMale:   string = ORKBiologicalSexIdentifierMale;   break;
@@ -66,7 +66,7 @@ ORKBloodTypeIdentifier const ORKBloodTypeIdentifierOPositive = @"HKBloodTypeOPos
 ORKBloodTypeIdentifier const ORKBloodTypeIdentifierONegative = @"HKBloodTypeONegative";
 
 NSString *ORKHKBloodTypeString(HKBloodType bloodType) {
-    NSString *string = @"";
+    NSString *string = nil;
     switch (bloodType) {
         case HKBloodTypeAPositive:  string = ORKBloodTypeIdentifierAPositive;   break;
         case HKBloodTypeANegative:  string = ORKBloodTypeIdentifierANegative;   break;
@@ -214,9 +214,8 @@ NSString *ORKHKBloodTypeString(HKBloodType bloodType) {
         }
     }
     
-    if (!_impliedAnswerFormat)
-        _impliedAnswerFormat = [ORKAnswerFormat textAnswerFormat];
-    
+    NSAssert(_impliedAnswerFormat, @"_impliedAnswerFormat should have been set");
+
     return _impliedAnswerFormat;
 }
 
