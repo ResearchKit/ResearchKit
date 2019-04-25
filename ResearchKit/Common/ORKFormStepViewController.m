ad5b4cb2fdd684042e0f3177276df4dfaa6bfdc8
Eric Schramm <schramm@careevolution.com>
2019-04-24 21:44:08 -0500

    update for renaming to hidePredicate

--- ResearchKit/Common/ORKFormStepViewController.m
+++ ResearchKit/Common/ORKFormStepViewController.m
@@ -710,8 +710,8 @@
 - (BOOL)allNonOptionalFormItemsHaveAnswers {
     ORKTaskResult *taskResult = self.taskViewController.result;
     for (ORKFormItem *item in [self formItems]) {
-        BOOL hideFormItem = [item.hideItemPredicate evaluateWithObject:@[taskResult]
-                                                 substitutionVariables:@{ORKResultPredicateTaskIdentifierVariableName : taskResult.identifier}];
+        BOOL hideFormItem = [item.hidePredicate evaluateWithObject:@[taskResult]
+                                             substitutionVariables:@{ORKResultPredicateTaskIdentifierVariableName : taskResult.identifier}];
         if (!item.optional && !hideFormItem) {
             id answer = _savedAnswers[item.identifier];
             if (ORKIsAnswerEmpty(answer) || ![item.impliedAnswerFormat isAnswerValid:answer]) {
@@ -759,8 +759,8 @@
     NSMutableIndexSet *sectionsToDelete = [NSMutableIndexSet indexSet];
     
     [formItems enumerateObjectsUsingBlock:^(ORKFormItem * _Nonnull formItem, NSUInteger idx, BOOL * _Nonnull stop) {
-        BOOL hideFormItem = [formItem.hideItemPredicate evaluateWithObject:@[taskResult]
-                                                     substitutionVariables:@{ORKResultPredicateTaskIdentifierVariableName : taskResult.identifier}];
+        BOOL hideFormItem = [formItem.hidePredicate evaluateWithObject:@[taskResult]
+                                                 substitutionVariables:@{ORKResultPredicateTaskIdentifierVariableName : taskResult.identifier}];
         ORKTableSection *section = _allSections[idx];
         if (!hideFormItem) {
             [_sections addObject:section];
