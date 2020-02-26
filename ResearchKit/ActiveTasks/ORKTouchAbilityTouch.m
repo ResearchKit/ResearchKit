66eed82afefb54c0ebf8deedf0a4150e2dcdaf19
srinath_muralidharan <srinath_muralidharan@apple.com>
2020-02-25 16:14:45 -0800

    fixing build error

--- ResearchKit/ActiveTasks/ORKTouchAbilityTouch.m
+++ ResearchKit/ActiveTasks/ORKTouchAbilityTouch.m
@@ -214,6 +214,15 @@
         case UITouchPhaseStationary:
             phaseString = @"stationary";
             break;
+        case UITouchPhaseRegionEntered:
+            phaseString = @"entered";
+            break;
+        case UITouchPhaseRegionMoved:
+            phaseString = @"moved";
+            break;
+        case UITouchPhaseRegionExited:
+            phaseString = @"exited";
+            break;
     }
     
     return [NSString stringWithFormat:@"<%@: %p; phase: %@; timestamp: %.6f; location: (%@, %@)>", self.class.description, self, phaseString, self.timestamp, @(self.locationInWindow.x), @(self.locationInWindow.y)];
