e70b7303817ae58c155fa4684556d1899d6271d4
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-05-18 14:12:08 -0700

    Update Setup

--- Package.swift
+++ Package.swift
@@ -1,25 +1,38 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.10
 import PackageDescription
 
 
 let package = Package(
     name: "ResearchKit",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v17),
+        .visionOS(.v1)
     ],
     products: [
         .library(name: "ResearchKit", targets: ["ResearchKit"]),
+        .library(name: "ResearchKitUI", targets: ["ResearchKitUI"]),
+        .library(name: "ResearchKitActiveTask", targets: ["ResearchKitActiveTask"]),
         .library(name: "ResearchKitSwiftUI", targets: ["ResearchKitSwiftUI"])
     ],
     targets: [
         .binaryTarget(
             name: "ResearchKit",
-            path: "./ResearchKit.xcframework"
+            path: "./Sources/ResearchKit.xcframework"
+        ),
+        .binaryTarget(
+            name: "ResearchKitUI",
+            path: "./Sources/ResearchKitUI.xcframework"
+        ),
+        .binaryTarget(
+            name: "ResearchKitActiveTask",
+            path: "./Sources/ResearchKitActiveTask.xcframework"
         ),
         .target(
             name: "ResearchKitSwiftUI",
             dependencies: [
-                .target(name: "ResearchKit")
+                .target(name: "ResearchKit"),
+                .target(name: "ResearchKitUI"),
+                .target(name: "ResearchKitActiveTask", condition: .when(platforms: [.iOS]))
             ]
         )
     ]
---
+++ Sources/ResearchKitSwiftUI/Export.swift
@@ -0,0 +1,13 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Group open-source organization
+//
+// SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
+@_exported import ResearchKit
+@_exported import ResearchKitUI
+#if TARGET_OS_IOS
+@_exported import ResearchKitActiveTasks
+#endif
