5a93506e0dc150d05c763c5ae66a73e7dd71a845
Andrew Plummer <andrew_plummer@apple.com>
2024-09-17 11:51:58 -0400

    Autoscroll enabled flag is not checked when determining if scrolling should occur

--- ResearchKitUI/Common/Step/Form Step/ORKFormStepViewController.m
+++ ResearchKitUI/Common/Step/Form Step/ORKFormStepViewController.m
@@ -1528,7 +1528,7 @@ NSString * const ORKFormStepViewAccessibilityIdentifier = @"ORKFormStepView";
                    
                    NSIndexPath *currentFirstResponderCellIndex = [tableView indexPathForCell:_currentFirstResponderCell];
                    
-                   if (currentFirstResponderCellIndex) {
+                   if (currentFirstResponderCellIndex && [self _isAutoScrollEnabled]) {
                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, DelayBeforeAutoScroll * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
                            [tableView scrollToRowAtIndexPath:currentFirstResponderCellIndex atScrollPosition:UITableViewScrollPositionBottom animated:YES];
                        });
